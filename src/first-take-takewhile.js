5b054bab28774fdd9317301abbfeb16f621b5568
Antonio Aguilar Herrera <antonio.herrera@live.com>
2019-07-17 15:04:04 -0500

    Ejemplo con First, Take y TakeWhile

---
+++ src/first-take-takewhile.js
@@ -0,0 +1,30 @@
+import { range, of, fromEvent } from 'rxjs';
+import { map, first, take, takeWhile } from 'rxjs/operators'
+
+export function example8() {
+    /** start coding */
+    /* Utility to show content into the view */
+    const displayLog = (content)=> {
+        let element = document.createElement('div');
+        element.innerHTML = content;
+        const logContainer = document.getElementById("first-take-takewhile");
+        logContainer.appendChild(element);
+    }
+
+    const grid = document.getElementById('grid');
+    const clicks = fromEvent(grid, 'click').pipe(
+        map( val => [
+            Math.floor(val.offsetX/50),
+            Math.floor(val.offsetY/50)
+        ]),
+        // first(),
+        // take(3),
+        takeWhile(val => val[0] > 3)
+    );
+    const subscription = clicks.subscribe(evt => {
+        displayLog(evt)
+    })
+
+
+    /** end coding */
+}
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { example4 } from './interval-timer';
 import { example5 } from './from-event';
 import { example6 } from './map-mapto-filter';
 import { example7 } from './tap';
+import { example8 } from './first-take-takewhile';
 
 //run stuff
 example();
@@ -15,3 +16,4 @@ example4();
 example5();
 example6();
 example7();
+example8();
